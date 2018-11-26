0d1eec82a0af2a027225e92a9f73b56afd85965b
Leandro Zago <leandrozago.dev@gmail.com>
2018-11-26 02:18:36 -0200

    cross map

--- zebra-product-app/src/main/java/br/com/zebra/product/Application.java
+++ zebra-product-app/src/main/java/br/com/zebra/product/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -17,6 +21,17 @@ public class Application extends SpringBootServletInitializer {
                 MongoConfiguration.class,
                 SwaggerConfiguration.class)
                 .run(args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 
     @Override
